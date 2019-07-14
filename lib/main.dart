7b8bde29c3c7b1607a91330887a7a2513723c696
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-07-14 08:56:50 +0500

    Added new stateful widget - but not running

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,27 @@ class MainWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text("Hello");
+    return SampleWidget();
   }
 
 }
+
+
+class SampleWidget extends StatefulWidget{
+  @override
+  SampleWidgetState createState() => SampleWidgetState();
+
+}
+
+class SampleWidgetState extends State<SampleWidget>{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        child: Text('State Button'),
+      ),
+    );
+  }
+}
+
+
